0a9ac0bdc0a264c5bbec48801849934d6365cdcc
Tim Struthoff <tim.struthoff@gmail.com>
2018-11-16 17:10:41 +0100

    Remove rast of game logic from client

--- src/ConnectFour/client/ConnectFourGame.java
+++ src/ConnectFour/client/ConnectFourGame.java
@@ -14,28 +14,12 @@ public class ConnectFourGame {
     private NetworkingClient client;
     private GameWindow window;
 
-    private String[][] playingField = new String[7][7];
-    private int numberOfMarks;
-
     private List<String> playerNames = new ArrayList<String>();
     private String myName;
-
-    private boolean gameStarted;
-
-    private String gameResult;
 
     public ConnectFourGame(NetworkingClient pClient, GameWindow pWindow) {
         client = pClient;
         window = pWindow;
-
-        gameStarted = false;
-        numberOfMarks = 0;
-
-        for (int column = 0; column < 7; column++) {
-            for (int row = 0; row < 7; row++) {
-                playingField[column][row] = " ";
-            }
-        }
 
         this.startGame();
 
@@ -46,8 +30,6 @@ public class ConnectFourGame {
         // Ask for player name and then send it to the server.
         myName = window.askForPlayerName();
         client.sendPlayerName(myName);
-
-        gameStarted = true;
     }
 
     public void addPlayer(String pName) {
@@ -62,22 +44,7 @@ public class ConnectFourGame {
      * @param pColumn The column where the mark should be set.
      * @param pRow The row where the mark should be set.
      */
-    public void setMark(int pColumn, int pRow, int pPlayerNumber) {
-        /*if (playingField[pColumn][pRow].equals(" ")) {
-            if (currentPlayer == playerOne) {
-                playingField[pColumn][pRow] = "X";
-                numberOfMarks++;
-                this.switchPlayers();
-            } else if (currentPlayer == playerTwo) {
-                playingField[pColumn][pRow] = "O";
-                numberOfMarks++;
-                this.switchPlayers();
-            } else {
-                System.err.println("Error Resetting: no current player!");
-            }
-        } else {
-            System.err.println("Error setting mark: Cell already marked");
-        }*/
+    public void setMark(int pColumn, int pRow) {
 
     }
 
@@ -91,19 +58,6 @@ public class ConnectFourGame {
      */
     public void drop(int pColumn) {
         client.sendDrop(pColumn);
-    }
-
-    public void gameEnd(boolean pWon) {
-        gameResult = "The game is still running!";
-        if (pWon) {
-            gameStarted = false;
-            // gameResult = "The game is over! Winner is: " + currentPlayer.getName();
-        } else {
-            if (numberOfMarks == 49) {
-                gameResult = "The game is over! Draw, there is no winner!";
-            }
-
-        }
     }
 
     /**
